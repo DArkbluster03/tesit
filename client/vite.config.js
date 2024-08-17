9b5211beae36a84ed044b7836813fec040929a55
DArkbluster03 <darkbluster03@gmail.com>
2024-08-17 22:44:15 +0600

    solution

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api-alpha-fawn.vercel.app/',
+        target:  'https://api-alpha-fawn.vercel.app/',
         secure: false,
       },
     },
