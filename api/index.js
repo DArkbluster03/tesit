843b7552f794adf479e917d0630b0cb55967ea3a
DArkbluster03 <darkbluster03@gmail.com>
2024-08-17 20:23:13 +0600

    reverse change

--- api/index.js
+++ api/index.js
@@ -32,26 +32,24 @@ const app = express();
 app.use(helmet());
 
 // CORS configuration
-app.use(cors());  // Customize as needed
+app.use(cors(
+  
+));
 
 app.use(express.json());
 app.use(cookieParser());
 
-// API routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-// Serve index.html for all other routes (client-side routing)
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+app.get('/test', (req, res) => {
+  res.send("Hello world");
 });
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -62,8 +60,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
