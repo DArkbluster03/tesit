bf173bc1dfcbad4f6722bc8ba9c6b0a25972f3e9
DArkbluster03 <darkbluster03@gmail.com>
2024-08-18 12:14:55 +0600

    aasndaqpoejwpqojepowqje

--- api/index.js
+++ api/index.js
@@ -52,8 +52,8 @@ app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-app.get('/test', (req, res) => {
-  res.send("Hello world");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
