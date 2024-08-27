40e17b3e4c9955731f901d1266739b36ca90b9f8
DArkbluster03 <darkbluster03@gmail.com>
2024-08-27 18:21:35 +0600

    FIXer1

--- api/index.js
+++ api/index.js
@@ -24,8 +24,8 @@ mongoose
   });
 
 // Define __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
 const app = express();
 
