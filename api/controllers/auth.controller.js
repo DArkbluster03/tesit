bd1861954387270d90866e1ea753cc0a8b6eb0df
DArkbluster03 <darkbluster03@gmail.com>
2024-08-29 19:56:36 +0600

    fixingng

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,18 +3,27 @@ import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user._id, isAdmin: user.isAdmin },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' } // Access token expires in 1 hour
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user._id, isAdmin: user.isAdmin },
+    process.env.JWT_REFRESH_SECRET, // Use a different secret for refresh tokens
+    { expiresIn: '7d' } // Refresh token expires in 7 days
+  );
+};
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === '' ||
-    email === '' ||
-    password === ''
-  ) {
-    next(errorHandler(400, 'All fields are required'));
+  if (!username || !email || !password || username === '' || email === '' || password === '') {
+    return next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,9 +36,24 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.json('Signup successful');
+
+    const accessToken = generateAccessToken(newUser);
+    const refreshToken = generateRefreshToken(newUser);
+
+    newUser.refreshToken = refreshToken;
+    await newUser.save();
+
+    const { password, ...rest } = newUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .json({ ...rest, refreshToken });
   } catch (error) {
-    next(error);
+    return next(error);
   }
 };
 
@@ -37,7 +61,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === '' || password === '') {
-    next(errorHandler(400, 'All fields are required'));
+    return next(errorHandler(400, 'All fields are required'));
   }
 
   try {
@@ -45,71 +69,66 @@ export const signin = async (req, res, next) => {
     if (!validUser) {
       return next(errorHandler(404, 'User not found'));
     }
+
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, 'Invalid password'));
     }
-    const token = jwt.sign(
-      { id: validUser._id, isAdmin: validUser.isAdmin },
-      process.env.JWT_SECRET
-    );
 
-    const { password: pass, ...rest } = validUser._doc;
+    const accessToken = generateAccessToken(validUser);
+    const refreshToken = generateRefreshToken(validUser);
+
+    validUser.refreshToken = refreshToken;
+    await validUser.save();
+
+    const { password, ...rest } = validUser._doc;
 
     res
       .status(200)
-      .cookie('access_token', token, {
+      .cookie('access_token', accessToken, {
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
       })
-      .json(rest);
+      .json({ ...rest, refreshToken });
   } catch (error) {
-    next(error);
+    return next(error);
+  }
+};
+export const refreshToken = async (req, res, next) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return next(errorHandler(401, 'Refresh token is required'));
+  }
+
+  try {
+    const user = await User.findOne({ refreshToken: token });
+
+    if (!user) {
+      return next(errorHandler(403, 'Refresh token is not valid'));
+    }
+
+    jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+      if (err || user._id.toString() !== decoded.id) {
+        return next(errorHandler(403, 'Refresh token is not valid'));
+      }
+
+      const newAccessToken = generateAccessToken(user);
+      const newRefreshToken = generateRefreshToken(user);
+
+      user.refreshToken = newRefreshToken;
+      user.save();
+
+      res
+        .status(200)
+        .cookie('access_token', newAccessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+        })
+        .json({ refreshToken: newRefreshToken });
+    });
+  } catch (error) {
+    return next(error);
   }
 };
 
-export const google = async (req, res, next) => {
-  const { email, name, googlePhotoUrl } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      const token = jwt.sign(
-        { id: user._id, isAdmin: user.isAdmin },
-        process.env.JWT_SECRET
-      );
-      const { password, ...rest } = user._doc;
-      res
-        .status(200)
-        .cookie('access_token', token, {
-          httpOnly: true,
-        })
-        .json(rest);
-    } else {
-      const generatedPassword =
-        Math.random().toString(36).slice(-8) +
-        Math.random().toString(36).slice(-8);
-      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-      const newUser = new User({
-        username:
-          name.toLowerCase().split(' ').join('') +
-          Math.random().toString(9).slice(-4),
-        email,
-        password: hashedPassword,
-        profilePicture: googlePhotoUrl,
-      });
-      await newUser.save();
-      const token = jwt.sign(
-        { id: newUser._id, isAdmin: newUser.isAdmin },
-        process.env.JWT_SECRET
-      );
-      const { password, ...rest } = newUser._doc;
-      res
-        .status(200)
-        .cookie('access_token', token, {
-          httpOnly: true,
-        })
-        .json(rest);
-    }
-  } catch (error) {
-    next(error);
-  }
-};
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+import { errorHandler } from '../utils/error.js';
+
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    if (!token) {
-      return next(errorHandler(401, 'Unauthorized'));
-    }
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) {
-        return next(errorHandler(401, 'Unauthorized'));
-      }
-      req.user = user;
-      next();
-    });
-  };
-  
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(errorHandler(401, 'Access denied. No token provided.'));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    next(errorHandler(401, 'Invalid token.'));
+  }
+};
