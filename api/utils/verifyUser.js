9c1f48b1691a1dc690215d195b557ccb532c99cd
DArkbluster03 <darkbluster03@gmail.com>
2024-08-29 13:47:57 +0600

    eoiqwhiuqwghwgfrefouerfgeruguergfeberghe

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,17 +1,16 @@
 import jwt from 'jsonwebtoken';
-import { errorHandler } from '../utils/error.js';
-
+import { errorHandler } from './error.js';
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return next(errorHandler(401, 'Access denied. No token provided.'));
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    next(errorHandler(401, 'Invalid token.'));
-  }
-};
+    const token = req.cookies.access_token;
+    if (!token) {
+      return next(errorHandler(401, 'Unauthorized'));
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return next(errorHandler(401, 'Unauthorized'));
+      }
+      req.user = user;
+      next();
+    });
+  };
+  
