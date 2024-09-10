45ca008bae422aa45e4b0cfecffac8fd3f860c36
DArkbluster03 <darkbluster03@gmail.com>
2024-09-11 03:44:24 +0600

    sadhkasdkjasdjkasdj

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api-alpha-fawn.vercel.app',
+      'https://api-alpha-fawn.vercel.app/api': {
+      
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
